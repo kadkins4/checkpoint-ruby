37d428f348b63397c4c25cabc71a22a9d7df6f6c
Kendall <adkins.kendall90@gmail.com>
2017-07-24 09:28:29 -0400

    fundamentals

--- fundamentals.rb
+++ fundamentals.rb
@@ -1,13 +1,12 @@
+require "pry"
 # Question 1
 # Define a method called `offer_rose`, which should take one argument named `person` (String).
 # When called the method should print to the terminal:
 # "Would you take this rose, `person`, in exchange for giving an old beggar woman shelter from the bitter cold?"
 # Type your solution directly below this line:
-
-
-
-
-
+def offer_rose(person)
+  "Would you take this rose, #{person}, in exchange for giving an old beggar woman shelter from the bitter cold?"
+end
 
 # Question 2
 # Assume the following hash...
@@ -25,10 +24,14 @@ town = {
 # - Remove "Belle" from `residents`
 # - Add "Belle" to the `guests` array
 # Type your solution directly below this line:
-
-
-
-
+def remove_belle
+  town[:residents][1].remove!
+end
+def belle_guest
+  town[:guests] << "Belle"
+end
+puts town[:residents]
+puts town[:guests]
 
 
 
@@ -36,10 +39,14 @@ town = {
 # Assume you have an array of strings representing friends' names...
 
 friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
-
 # Using a loop and string interpolation, print each string in `friends` to the Terminal...
 # "Belle is friends with Chip Potts"
 # "Belle is friends with Cogsworth"
 # "Belle is friends with Lumière"
 # "Belle is friends with Mrs. Potts"
 # Type your solution directly below this line:
+friends.each do |person|
+  puts "Belle is friends with #{person}"
+end
+
+binding.pry
