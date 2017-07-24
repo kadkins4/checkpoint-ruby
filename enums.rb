6fce259f3b14f762ad5748ed4bb35588e5941b2d
Kendall <adkins.kendall90@gmail.com>
2017-07-24 09:28:42 -0400

    enums

--- enums.rb
+++ enums.rb
@@ -12,10 +12,10 @@ lost_boys = [
 # Use `.each` to iterate over the `lost_boys` array and increase each boy's age by
 # 30 years.
 # Type your solution directly below this line:
-
-
-
-
+lost_boys.each do |boy|
+  boy[:age] += 30
+end
+puts lost_boys
 
 
 # Question 5
@@ -27,3 +27,5 @@ children = ['Wendy', 'John', 'Michael']
 # of their names. Assign the returned array to a variable called `darling_children`.
 # Example: `Wendy` should become `Wendy Darling` in the new array.
 # Type your solution directly below this line:
+darling_children = children.map { |darling| darling + " Darling" }
+puts darling_children
